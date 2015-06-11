d791b5d23bd9e8c740699b1fb7d8c87d32f18d8a
Martin Andrews <GitHub@mdda.net>
2015-06-11 16:37:22 +0800

    Added outputfilter script

---
+++ bin/ipynb_output_filter.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python
+
+import sys
+
+version = None
+
+print "HELLO OUTPUT_FILTER"
+
+if sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+try:
+    # Jupyter
+    from jupyter_nbformat import reads, write
+except ImportError:
+    try:
+        # New IPython
+        from IPython.nbformat import reads, write
+    except ImportError:
+        # Old IPython
+        from IPython.nbformat.current import reads, write
+        version = 'json'
+
+to_parse = sys.stdin.read()
+
+if not version:
+    import json
+    json_in = json.loads(to_parse)
+    version = json_in['nbformat']
+
+json_in = reads(to_parse, version)
+
+if hasattr(json_in, 'worksheets'):
+    # IPython
+    sheets = json_in.worksheets
+else:
+    # Jupyter
+    sheets = [json_in]
+
+for sheet in sheets:
+    for cell in sheet.cells:
+        if "outputs" in cell:
+            cell.outputs = []
+        for field in ("prompt_number", "execution_number"):
+            if field in cell:
+                del cell[field]
+        for field in ("execution_count",):
+            if field in cell:
+                cell[field] = None
+
+if 'signature' in json_in.metadata:
+    json_in.metadata['signature'] = ""
+
+write(json_in, sys.stdout, version)
