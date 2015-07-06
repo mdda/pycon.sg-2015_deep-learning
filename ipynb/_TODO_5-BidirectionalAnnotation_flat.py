3e7adf553521ef5021f5e918892451e1dbd631a8
Martin Andrews <GitHub@mdda.net>
2015-07-06 18:01:03 +0800

    Now have larger embedding

--- ipynb/_TODO_5-BidirectionalAnnotation_flat.py
+++ ipynb/_TODO_5-BidirectionalAnnotation_flat.py
@@ -34,7 +34,8 @@ theano.config.compute_test_value = 'raise'
 theano.config.optimizer='fast_compile'
 
 import hickle
-word2vec = hickle.load('/home/andrewsm/SEER/services/deepner/server/data/embedding.0.hickle')
+#word2vec = hickle.load('/home/andrewsm/SEER/services/deepner/server/data/embedding.0.hickle')
+word2vec = hickle.load('/home/andrewsm/SEER/services/deepner/server/data/embedding.20.hickle')
 embedding = word2vec['embedding']
 code2word = word2vec['vocab']
 
